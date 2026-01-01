825652002749a0e9b5db7d2ccd6290eeeaac791d
이지연 <wldus019113@icloud.com>
2026-01-01 17:50:32 +0900

    - 동전2 문제 풀이

--- src/A5DP/Baekjoon/G2294동전2.java
+++ src/A5DP/Baekjoon/G2294동전2.java
@@ -3,6 +3,7 @@ package A5DP.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class G2294동전2 {
@@ -19,5 +20,25 @@ public class G2294동전2 {
         // k원까지 만들 때 동전의 수를 담는 dp
         // dp = new int[k+1];
         // 1원 : 1(1개), 2원: 1+1(2개), 3원: 1+1+1(3개), 4원: 1+1+1+1(4개), ..., 15원: 14를 만드는 경우 + 1, 10을 만드는 경우 +1, 3을 만드는 경우 +1
+
+        int[] coin = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = 100_001;
+        int[] dp = new int[k + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            int c = coin[i];
+            for (int j = c; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - c] + 1);
+            }
+        }
+
+        System.out.println(dp[k] == max ? -1 : dp[k]);
     }
 }
